99a10fea542413f8614456fa188378dd54d2a505
BradleyGilden <bradleygilden@gmail.com>
2024-03-15 22:13:54 +0200

    authentication with bcrypt

---
+++ backend/src/auth/index.js
@@ -0,0 +1,28 @@
+/**
+ * handles authentication of a users log in
+ */
+import bcrypt from 'bcrypt';
+
+/**
+ * hashes a plaintext password using bcrypts hash method with 10 satl rounds
+ * @param {string} plainTextPassword plain text password
+ * @return {string} a salted hash
+ */
+const hashpwd = (plainTextPassword) => {
+  return bcrypt.hash(plainTextPassword, 10);
+}
+
+/**
+ * validates a plaintext password against a salted hash
+ * @param {string} plainTextPassword plain text password
+ * @param {string} hash a salted hash
+ * @return {boolean} true if password matches, false otherwise
+ */
+const checkpwd = (plainTextPassword, hash) => {
+  return bcrypt.compare(plainTextPassword, hash);
+}
+
+export {
+  checkpwd,
+  hashpwd,
+}
