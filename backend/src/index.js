c61e6aa1ab6a792dfc80d2eac7771b6fa4fbaddd
BradleyGilden <bradleygilden@gmail.com>
2024-03-15 23:05:44 +0200

    integrating middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import baseRouter from './routes';
 import mongooseConnect from './db/connect';
-import dotenv from 'dotenv';
+import errorHandler from './middleware/errorHandler';
 
 // get environment variable files form .env for testing
 dotenv.config();
@@ -10,10 +12,14 @@ dotenv.config();
 const app = express();
 
 // Middleware declarations
-app.use(morgan("dev"))
+app.use(cors());
+app.use(morgan("dev"));
 app.use('/api', baseRouter);
+app.use(errorHandler);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log("server starting on port 3000");
   // establish database connection
   mongooseConnect();
