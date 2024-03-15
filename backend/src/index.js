ccda21d98fe4868a64cf88b298f1284310ceedb5
BradleyGilden <bradleygilden@gmail.com>
2024-03-15 17:06:27 +0200

    dev branch up

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import baseRouter from './routes';
+import morgan from 'morgan';
 
 const app = express();
 
 // Middleware declarations
-
+app.use(morgan("dev"))
 app.use('/api', baseRouter);
 
 app.listen(3000, () => console.log("server starting on port 3000"));
