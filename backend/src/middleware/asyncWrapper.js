3e0d9372aa6b84776ee7e85e8611af50ba70d3b3
BradleyGilden <bradleygilden@gmail.com>
2024-03-15 15:23:53 +0200

    asynchronous wrapper for api controllers

---
+++ backend/src/middleware/asyncWrapper.js
@@ -0,0 +1,15 @@
+/**
+ * Wraps asynchronous methods, mainly those making external api requests, to handle errors
+ * @param {asyncCallBack} callBack 
+ * @returns {Promise<null>}
+ */
+
+const asyncWrapper = (callBack) => {
+  return async (req, res, next) => {
+    try {
+      await callBack(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
