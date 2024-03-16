0118435943100e2b42118a98a612def1984bc868
BradleyGilden <bradleygilden@gmail.com>
2024-03-16 21:47:50 +0200

    Login and Signup Pages

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,16 @@
 <script>
+  import Router from "svelte-spa-router";
+  import Login from "./routes/Login.svelte";
+  import Home from "./routes/Home.svelte";
+
+  const routes = {
+    '/': Login,
+    '/home': Home,
+  }
 </script>
 
 <main>
+  <Router { routes } />
 </main>
 
 <style>
---
+++ frontend/src/routes/Login.svelte
@@ -0,0 +1,26 @@
+<script>
+// @ts-nocheck
+
+  import ThemeSelect from "../components/ThemeSelect.svelte";
+  import LoginForm from "../components/LoginForm.svelte";
+  import SignupForm from "../components/SignupForm.svelte";
+
+  let showLogin = true;
+  const handleFormSwitch = () => {showLogin = !showLogin};
+</script>
+
+<div>
+  <header class='flex justify-center items-center mt-10'>
+    <h1 class='text-[5rem] font-mserrat text-center text-primary'>Babel Chat</h1>
+    <div class='ml-10'>
+      <ThemeSelect/>
+    </div>
+  </header>
+  {#if showLogin}
+  <LoginForm on:click={handleFormSwitch} />
+  {:else}
+  <SignupForm on:click={handleFormSwitch} />
+  {/if}
+</div>
+<style>
+</style>
